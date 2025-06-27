ff21da301fcc814c2fecccfe66aa637cf65c123c
Shahzeb-Abro <khizr772@gmail.com>
2025-06-27 15:55:13 +0500

    Enabled CI/CD pipeline

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ dotenv.config();
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("Deployment successful");
+  res.send("Deployment successful 1");
 });
 
 app.listen(process.env.PORT, () => {
